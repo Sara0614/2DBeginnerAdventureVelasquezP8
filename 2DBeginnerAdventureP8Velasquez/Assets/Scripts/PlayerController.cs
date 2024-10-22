603d04043152142113cf9acce24c5e29d815f1c6
Sara0614 <euniceisnot10@gmail.com>
2024-10-22 15:26:28 -0700

    Added stuff to my 2DAdventure
    I added tiles and made the player move

--- 2DBeginnerAdventureP8Velasquez/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Velasquez/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControler : MonoBehaviour
 {
+    public InputAction LeftAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+
         transform.position = position;
     }
+
 }
