0666ddc90d351b74d50f70f63bc6b72acb24ed83
Sara0614 <euniceisnot10@gmail.com>
2024-10-30 15:24:34 -0700

    Added some stuff to my 2DAdventure
    I added some world Interactions like Damage zones and collectables

--- 2DBeginnerAdventureP8Velasquez/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Velasquez/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+        float invincibleTimer;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -12,22 +22,46 @@ public class PlayerControler : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-         horizontal = Input.GetAxis("Horizontal");
-         vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if(isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0 )
+            {
+                isInvincible = false;
+            }
+        }
 
     }
-     void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime; ;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime; ;
 
         rigidbody2d.MovePosition(position);
     }
-
+    public void ChangeHealth(int amount)
+    {
+        if(amount <0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
