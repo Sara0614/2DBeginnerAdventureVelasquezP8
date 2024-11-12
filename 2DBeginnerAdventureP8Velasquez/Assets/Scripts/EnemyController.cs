9cecb190c53f4213d95ef78801992cde2fe95a01
Sara0614 <euniceisnot10@gmail.com>
2024-11-12 15:26:29 -0800

    I added some stuff
    I added healthbar.

--- 2DBeginnerAdventureP8Velasquez/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Velasquez/Assets/Scripts/EnemyController.cs
@@ -9,6 +9,9 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 1.0f;
 
+    public ParticleSystem smokeEffect;
+
+
     Rigidbody2D rigidbody2d;
 
     bool broken = true;
@@ -78,5 +81,6 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2d.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
---
+++ 2DBeginnerAdventureP8Velasquez/Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealthBar : MonoBehaviour
+{
+    public Image mask;
+    float originalSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
